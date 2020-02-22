cab0054098f659ccab4ce890cedc7f0d79d89b94
MVP <dima.bobryshev.mvp@gmail.com>
2020-02-22 04:27:32 +0200

    quickSort 1/2

---
+++ src/main/java/com/mvp/tasks/Start.java
@@ -0,0 +1,13 @@
+package com.mvp.tasks;
+
+import static com.mvp.tasks.QuickSort.*;
+
+public class Start {
+    public static void main(String[] args) {
+        setArrayLength(10);
+        initArray();
+        printArray();
+        quickSort();
+        printArray();
+    }
+}
